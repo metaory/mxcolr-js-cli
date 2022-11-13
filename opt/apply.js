a45741ade27a3df6eef6b194816501e917348e3e
metaory <metaory@gmail.com>
2022-11-13 21:20:44 +0800

    tpl missmatch wip

--- opt/apply.js
+++ opt/apply.js
@@ -3,7 +3,7 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import pupa from 'pupa'
 
-export default async (palette) => {
+export default async(palette) => {
   console.log('APPLY')
 
   const getPath = (path) => resolve(
