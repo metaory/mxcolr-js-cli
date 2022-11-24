a5e92dd1c0ba87c8a2d22eb1ba53f3dc5a758295
metaory <metaory@gmail.com>
2022-11-24 10:49:59 +0800

    apply class rewrite

--- opt/apply.js
+++ opt/apply.js
@@ -3,98 +3,99 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import pupa from 'pupa'
 
-// --------------------------------------------------------------------------
-const getPath = (path) => resolve(dirname(fileURLToPath(import.meta.url)), path)
-// --------------------------------------------------------------------------
 const printLine = () => console.log(C.grey(fillFrom('╸')))
 // --------------------------------------------------------------------------
-const keyGet = (action, tpl) => {
-  const key = action ?? tpl
-  if (key in cfg.templates === false) {
-    L.error('key is missing in templates: ' + key)
-  }
-  return key
-}
+const resolvePath = (path) => resolve(dirname(fileURLToPath(import.meta.url)), path)
 // --------------------------------------------------------------------------
-const parseOutput = async (key, outputPath, palette) => {
-  const tplPath = getPath(`../templates/${key}`)
-  const exist = fs.pathExistsSync(tplPath)
-  if (exist === false) {
-    L.error('tpl path doesnt exists:\n' + tplPath)
-  }
-  printLine()
-  const file = fs.readFileSync(tplPath, { encoding: 'utf8' })
-
-  const parsed = pupa(file, palette ?? process.env)
-
-  L.loading('input', tplPath)
-
-  return parsed
-}
-// --------------------------------------------------------------------------
-const tplsGet = async (action) => {
-  if (action) {
-    return [getPath(`../templates/${action}`)]
-  }
-  const tpls = [await autocompleteInput('tpl', ['all', ...Object.keys(cfg.templates)])]
-  const [{ tpl }] = tpls
-  if (tpl === 'all') {
-    const templates = Object.keys(cfg.templates)
-    return templates.map(x => ({ tpl: x }))
-  }
-  return tpls
-}
-// --------------------------------------------------------------------------
-const outputPathsGet = (key) => {
-  const { output } = cfg.templates[key]
-  return Array.isArray(output) ? output : [output]
-}
-// --------------------------------------------------------------------------
-const writeTplFile = (outputPath, parsed) => {
-  fs.writeFileSync(outputPath, parsed)
+const writeFile = (outputPath, data) => {
+  fs.writeFileSync(outputPath, data)
   L.info('done', outputPath)
 }
 // --------------------------------------------------------------------------
-const writeOutput = async(key, outputPath, parsed) => {
-  if (argv.f || argv.force) { return writeTplFile(outputPath, parsed) }
-
-  const { writeTpl } = await confirmInput('writeTpl', `apply ${key}?`, true)
-  if (writeTpl) {
-    writeTplFile(outputPath, parsed)
+class Apply {
+  #palette
+  #action
+  #template
+  constructor(palette = process.env, action) {
+    this.palette = palette
+    this.action = action
   }
-}
-// --------------------------------------------------------------------------
-export default async (palette) => {
-  const { _: [, action] } = argv
 
-  const tpls = await tplsGet(action)
+  get key() {
+    const key = this.action ?? this.template
+    if (key in cfg.templates === false) {
+      L.error('key is missing in templates: ' + key)
+    }
+    return key
+  }
 
-  for (const { tpl } of tpls) {
-    const key = keyGet(action, tpl)
-    const outputPaths = outputPathsGet(key)
-    for (const outputPath of outputPaths) {
-      const parsed = await parseOutput(key, outputPath, palette)
-      await writeOutput(key, outputPath, parsed)
+  get outputPaths() {
+    const { output } = cfg.templates[this.key]
+    return Array.isArray(output) ? output : [output]
+  }
+
+  get tpl() {
+    return this.template
+  }
+
+  set tpl(t) {
+    this.template = t
+  }
+
+  async tpls() {
+    if (this.action) {
+      return [resolvePath(`../templates/${this.action}`)]
+    }
+    const tpls = [
+      await autocompleteInput('tpl', ['all', ...Object.keys(cfg.templates)])
+    ]
+    const [{ tpl }] = tpls
+    if (tpl === 'all') {
+      return Object.keys(cfg.templates).map(x => ({ tpl: x }))
+    }
+    return tpls
+  }
+
+  get tplPath() {
+    const tplPath = resolvePath(`../templates/${this.key}`)
+    const exist = fs.pathExistsSync(tplPath)
+    if (exist === false) {
+      L.error('tpl path doesnt exists:\n' + tplPath)
+    }
+    return tplPath
+  }
+
+  parseOutput(outputPath) {
+    const file = fs.readFileSync(this.tplPath, { encoding: 'utf8' })
+    const parsed = pupa(file, this.palette)
+    L.loading('input', this.tplPath)
+    return parsed
+  }
+
+  async writeOutput(outputPath, parsed) {
+    if (argv.f || argv.force) { return writeFile(outputPath, parsed) }
+    const { writeTpl } = await confirmInput('writeTpl', `apply ${this.key}?`, true)
+    if (writeTpl) {
+      writeFile(outputPath, parsed)
     }
   }
 }
 // --------------------------------------------------------------------------
+export default async(palette) => {
+  const { _: [, action] } = argv
 
-// ##########################################################################
-// const templates = fs.readdirSync(getPath('../templates/'))
-// --------------------------------------------------------------------------
-// const { input, output } = cfg.templates[tpl]
-// const tplPath = getPath(`../templates/${input}`)
-// const file = fs.readFileSync(tplPath, { encoding: 'utf8' })
-//
-// const parsed = pupa(file, palette)
-//
-// const { writeTpl } = await confirmInput('writeTpl', `apply ${tpl}?`)
-// if (writeTpl) {
-//   fs.writeFileSync(output, parsed)
-// }
-// --------------------------------------------------------------------------
-// process.exit()
-// const templates = fs.readdirSync(getPath('../templates/'))
-// fs.ensureDir('/tmp/mxcolr')
+  const apply = new Apply(palette, action)
 
+  const tpls = await apply.tpls()
+
+  for (const { tpl } of tpls) {
+    apply.tpl = tpl
+    for (const outputPath of apply.outputPaths) {
+      printLine()
+      await apply.writeOutput(
+        outputPath,
+        apply.parseOutput(outputPath)
+      )
+    }
+  }
+}
